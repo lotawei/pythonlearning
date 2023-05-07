a0dd6f64759f0ab702050d5836eef5ddad68839c
lotawei <1286594907@qq.com>
2023-05-07 21:27:46 +0800

    Create practice.py
    test  爬虫使用

---
+++ 爬虫/practice.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get('https://www.baidu.com')
+print(response.status_code)
+print(response.text)
+print(response.headers)
+print(response.encoding)
